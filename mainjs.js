bc6d58b5120bcf6da24806f0722bde15c3212058
Alvyn Ly <alvyn.ly@gmail.com>
2017-04-05 13:39:33 -0700

    Update mainjs.js

--- mainjs.js
+++ mainjs.js
@@ -2,7 +2,10 @@ requirejs.config({
 	paths: {
 		DoT_leaflet: 'DoTLeaflet',
 		leaflet: 'leaflet',
-		esri_leaflet: 'esri_leaflet'
+		esri_leaflet: 'esri_leaflet',
+		googleAPI: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBimi-uaVPiKmbW53QUc61AOkzflR0XzZs&sensor=false&libraries=places',
+		smoothMarkerBouncing: 'smoothMarkerBouncing',
+		Leaflet_Google: 'Leaflet_Google'
 	}
 	//shim: {
 	//	'DoT_leaflet': {
@@ -14,5 +17,5 @@ requirejs.config({
 	//}
 });
 
-require(["DoT_leaflet", "leaflet", "esri_leaflet"]);
+require(["DoT_leaflet", "leaflet", "esri_leaflet", "googleAPI", "smoothMarkerBouncing", "Leaflet_Google"]);
 
