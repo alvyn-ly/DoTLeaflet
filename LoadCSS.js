ce9304fb37acd430d34869784c8a0df8c3cc08fa
Alvyn Ly <alvyn.ly@gmail.com>
2017-04-05 14:20:22 -0700

    Update LoadCSS.js

--- LoadCSS.js
+++ LoadCSS.js
@@ -1,6 +1,7 @@
     var cssId = 'DoTLeafletCSS';  // you could encode the css path itself to generate id..
     if (!document.getElementById(cssId))
     {
+        console.log("Test!");
     	var head  = document.getElementsByTagName('head')[0];
     	var link  = document.createElement('link');
     	link.id   = cssId;
