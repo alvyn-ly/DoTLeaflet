82160c7a978d27b9ed96ae2256f8bcb9540e590f
Alvyn Ly <alvyn.ly@gmail.com>
2017-04-26 14:51:07 -0700

    Update main.js

--- main.js
+++ main.js
@@ -9,8 +9,7 @@ requirejs.config({
 		esri_leaflet: 'esri_leaflet',
 		googleAPI: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBimi-uaVPiKmbW53QUc61AOkzflR0XzZs&sensor=false&libraries=places',
 		smoothMarkerBouncing: 'smoothMarkerBouncing',
-		Leaflet_Google: 'Leaflet_Google',
-		Routing: 'leaflet-routing-machine'
+		Leaflet_Google: 'Leaflet_Google'
 	},
 	shim: {
 		'DoT_leaflet': {
@@ -27,4 +26,4 @@ requirejs.config({
 
 
 
-require(["Routing","JQuery_ui", "JQuery", "LoadCSS", "DoT_leaflet", "leaflet", "esri_leaflet", "googleAPI", "smoothMarkerBouncing", "Leaflet_Google"]);
+require(["JQuery_ui", "JQuery", "LoadCSS", "DoT_leaflet", "leaflet", "esri_leaflet", "googleAPI", "smoothMarkerBouncing", "Leaflet_Google"]);
