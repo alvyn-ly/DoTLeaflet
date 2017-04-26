44acdcd5aacef84a12f4717085304987ef859683
Alvyn Ly <alvyn.ly@gmail.com>
2017-04-26 10:39:19 -0700

    Update DoTLeaflet.js

--- DoTLeaflet.js
+++ DoTLeaflet.js
@@ -52,6 +52,14 @@ define(['JQuery', 'JQuery_ui', 'leaflet'], function(JQuery) {
 			new L.Control.GoogleAutocomplete().addTo(map);
 		}
 
+		if (options.route){
+			L.Routing.control({
+				waypoints: [
+				L.latLng(57.74, 11.94),
+				L.latLng(57.6792, 11.949)
+				]
+			}).addTo(map);
+		}
 
 		if (options.layerMenu){
 			L.control.layers({
@@ -364,8 +372,8 @@ function googleReverseGeocode(passLat, passLng) {
         });
 }
     //END GOOGLE AUTO COMPLETE SCRIPT
-if (options != undefined){	
-	init(options);
-}
+    if (options != undefined){	
+    	init(options);
+    }
 })
 
