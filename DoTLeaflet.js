7b8d96ed32e8fbba4d2bb9faa44d7bfa5587bea5
Alvyn Ly <alvyn.ly@gmail.com>
2017-05-24 09:50:22 -0700

    added timeframe filter.

--- DoTLeaflet.js
+++ DoTLeaflet.js
@@ -12,9 +12,13 @@ define(['JQuery', 'JQuery_ui', 'leaflet'], function(JQuery) {
 		esriLayer = L.esri.basemapLayer('Topographic');
 		map = L.map( options.div ,{layers: esriLayer}).setView([37.33766995, -121.8874011], 16);
 
-		var projectArray = [];
+		var projectArray = []; 
 
 		var projQuery = "Select p.ZipCode__c, p.TrafficCalmingRequestType__c, p.TrafficCalmingProjectType__c, p.TrafficCalmingConcernType__c, p.TrafficCalmingConcernItem__c, p.TrafficAffected__c, p.SystemModstamp, p.Summary__c, p.Status__c,  p.StandardizedLocation__c, p.SignalTimeOfDay__c, p.SignalSideOfStreet__c, p.SignalProjectType__c, p.SignalProblemDirection__c, p.SignalOperationAssignmentCount__c, p.SignalOperationAssignmentCompleteCount__c, p.SignalFundAdjustment__c, p.SignalDesignAssignmentCount__c, p.SignalDesignAssignmentCompleteCount__c, p.SignalDayOfWeek__c, p.SignalCustomerSurveySent__c, p.SignAssignmentCount__c, p.SignAssignmentCompleteCount__c, p.School__c, p.RequesterNotificationDate__c, p.RecordTypeId, p.ReceiveDateTime__c, p.ProjectType__c, p.ProjectLink__c, p.ProjectDurationDays__c, p.Program__c, p.OwnerId, p.Name__c, p.Name, p.MarkingAssignmentCount__c, p.MarkingAssignmentCompleteCount__c, p.MapLocation__c, p.MajorProject__c, p.LastModifiedDate, p.LastModifiedById, p.LastActivityDate, p.IsDeleted, p.Investigator__c, p.Id, p.ITSAssignmentCount__c, p.ITSAssignmentCompleteCount__c, p.HoursSpent__c, p.HeavyEquipmentAssignmentCount__c, p.HeavyEquipmentAssignmentCompleteCount__c, p.GeometricProjectType__c, p.GeometricProjectSource__c, p.GeometricPlanNumber__c, p.Geolocation__Longitude__s, p.Geolocation__Latitude__s, p.ElectricalAssignmentCount__c, p.ElectricalAssignmentCompleteCount__c, p.CreatedDate, p.CreatedById, p.CouncilDistrict__c, p.Coordinator__c, p.Concern__c, p.ChargeNumber__c From Project__c p";
+		if (options.startDate != undefined && options.endDate != undefined){
+			var limit = " Where CreatedDate >= " + options.startDate.substring(0, 10) + " AND CreatedDate <= " + options.endDate.substring(0, 10);
+			projQuery = projQuery + limit;
+		}
 		var records = sforce.connection.query(projQuery);
 		var records1 = records.getArray('records');
 
@@ -223,17 +227,17 @@ define(['JQuery', 'JQuery_ui', 'leaflet'], function(JQuery) {
 					draggable: true,
 					icon: myIcon
 				}).on('dragend', function(event){
-            		try {
-            			googleReverseGeocode(markers[0].getLatLng().lat,markers[0].getLatLng().lng);
-            			getDropLocation(markers[0].getLatLng().lat, markers[0].getLatLng().lng, markers[0]);
-            		} catch (e) {
-            			if (options.error){
-            				console.log("Error", e.stack);
-            				console.log("Error", e.name);
-            				console.log("Error", e.message);
-            			}
-            		}
-            	}).addTo( map );
+					try {
+						googleReverseGeocode(markers[0].getLatLng().lat,markers[0].getLatLng().lng);
+						getDropLocation(markers[0].getLatLng().lat, markers[0].getLatLng().lng, markers[0]);
+					} catch (e) {
+						if (options.error){
+							console.log("Error", e.stack);
+							console.log("Error", e.name);
+							console.log("Error", e.message);
+						}
+					}
+				}).addTo( map );
 				console.log(markerCoords.lat + "   " + markerCoords.lng);  
 				try {
 					googleReverseGeocode(markers[0].getLatLng().lat,markers[0].getLatLng().lng);
